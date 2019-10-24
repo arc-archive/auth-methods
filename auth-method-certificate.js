05c13c8cded0f7c5f326625583264d80432c9589
Pawel <jarrodek@gmail.com>
2019-10-24 06:25:23 -0700

    chore(style): redesigning the list

--- auth-method-certificate.js
+++ auth-method-certificate.js
@@ -71,15 +71,24 @@ class AuthMethodCertificate extends ClientCertificatesConsumerMixin(AuthMethodBa
       anypoint-radio-button {
         width: 100%;
         margin: 8px 0;
+        align-items: flex-start;
+      }
+
+      .default {
+        align-items: center;
       }
 
       .name {
-        font-weight: 500;
+        font-size: 1rem;
+        font-weight: 400;
+        margin-bottom: 8px;
+        margin-top: 7px;
       }
 
       .created {
         font-size: 0.85rem;
         color: var(--auth-method-certificate-second-line-color, initial);
+        font-weight: 200;
       }
 
       .list {
@@ -109,26 +118,24 @@ class AuthMethodCertificate extends ClientCertificatesConsumerMixin(AuthMethodBa
         <anypoint-radio-button
           data-id="none"
           ?compatibility="${compatibility}"
+          class="default"
         >None</anypoint-radio-button>
         ${items.map((item) => html`<anypoint-radio-button
           data-id="${item._id}"
           ?compatibility="${compatibility}"
         >
-          <div class="button-content">
-            <div class="cert-type-ico">${item.type}</div>
-            <div class="cert-meta">
-              <span class="name">${item.name}</span>
-              <span class="created">Added:
-                <date-time
-                  .date="${item.created}"
-                  year="numeric"
-                  month="numeric"
-                  day="numeric"
-                  hour="numeric"
-                  minute="numeric"
-                ></date-time>
-              </span>
-            </div>
+          <div class="cert-meta">
+            <span class="name">${item.name}</span>
+            <span class="created">Added:
+              <date-time
+                .date="${item.created}"
+                year="numeric"
+                month="numeric"
+                day="numeric"
+                hour="numeric"
+                minute="numeric"
+              ></date-time>
+            </span>
           </div>
         </anypoint-radio-button>`)}
       </anypoint-radio-group>
